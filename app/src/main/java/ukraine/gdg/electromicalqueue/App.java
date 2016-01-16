8561991543b03578efb004c780b4869c1fab3664
zanctor <1tshark1@mail.ru>
2016-01-16 15:44:41 +0200

    Initial commit

---
+++ app/src/main/java/ukraine/gdg/electromicalqueue/App.java
@@ -0,0 +1,12 @@
+package ukraine.gdg.electromicalqueue;
+
+import android.app.Application;
+
+/**
+ * Created by Igor
+ * on 16.01.2016
+ */
+public class App extends Application {
+
+
+}
---
+++ app/src/main/java/ukraine/gdg/electromicalqueue/Constants.java
@@ -0,0 +1,11 @@
+package ukraine.gdg.electromicalqueue;
+
+/**
+ * Created by Igor
+ * on 16.01.2016
+ */
+public interface Constants {
+
+    String BASE_API = "";
+
+}
